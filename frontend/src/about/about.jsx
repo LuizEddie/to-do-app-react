73f6b63f51e6ba2ba1f1bf6ba0497c9d813c45e9
LuizEddie <contato.luiz1997@gmail.com>
2024-03-27 00:14:11 -0300

    Aula 145: Componentes Grid e IconButton

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageHeader from '../template/pageHeader';
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet tempora velit molestiae odio neque reiciendis aliquid veniam quia iure, nisi sint similique accusamus autem provident ipsam voluptatibus dolores magnam laborum.</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet tempora velit molestiae odio neque reiciendis aliquid veniam quia iure, nisi sint similique accusamus autem provident ipsam voluptatibus dolores magnam laborum.</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet tempora velit molestiae odio neque reiciendis aliquid veniam quia iure, nisi sint similique accusamus autem provident ipsam voluptatibus dolores magnam laborum.</p>
+    </div> 
+);
